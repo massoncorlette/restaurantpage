6178556821d79417efd22dc0395233edb5099ab0
massoncorlette <massoncorlette07@gmail.com>
2024-06-08 02:48:41 -0400

    Finished the semantics of this project, just need to finish the design.

--- src/contact.js
+++ src/contact.js
@@ -11,10 +11,18 @@ export function contactClick() {
   
     const title = document.createElement('div');
     title.id = 'title';
-    title.textContent = "Vitos's Pizza";
+    title.textContent = "Vito's Pizza";
     content.appendChild(title);
   
     const container = document.createElement('div');
+    const italy = document.createElement('img');
+    const address = document.createElement('p');
+    italy.src = italyArt;
+    italy.id = 'italyArt';
+    address.id = 'addressTxt';
+    address.textContent = "Vito's Pizzeria 1234 Napoli Way Little Italy, NY 10012 USA"
+    container.appendChild(italy);
+    container.appendChild(address);
     container.id = 'contactContainer';
     content.appendChild(container);
   
